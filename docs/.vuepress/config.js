cd41854c2095b2407fcd04bc71540461386a3837
nateiler <nate@flipboxdigital.com>
2019-01-10 10:37:16 -0700

    docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,17 +2,25 @@ module.exports = {
     title: 'Salesforce',
     description: 'Salesforce Plugin for Craft CMS',
     base: '/',
+    theme: 'flipbox',
     themeConfig: {
+        logo: '/icon.svg',
         docsRepo: 'flipboxfactory/craft-salesforce',
         docsDir: 'docs',
         docsBranch: 'master',
         editLinks: true,
         search: true,
         searchMaxSuggestions: 10,
+        codeLanguages: {
+            twig: 'Twig',
+            php: 'PHP',
+            json: 'JSON',
+            // any other languages you want to include in code toggles...
+        },
         nav: [
-            {text: 'Details', link: 'https://flipboxdigital.com/software/force'},
-            {text: 'Changelog', link: 'https://github.com/flipboxfactory/craft-salesforce/blob/develop/CHANGELOG'},
-            {text: 'Documentation', link: '/'}
+            {text: 'Documentation', link: 'https://salesforce.flipboxfactory.com'},
+            {text: 'Changelog', link: 'https://github.com/flipboxfactory/craft-salesforce/blob/master/CHANGELOG.md'},
+            {text: 'Repo', link: 'https://github.com/flipboxfactory/craft-salesforce'}
         ],
         sidebar: {
             '/': [
@@ -21,19 +29,19 @@ module.exports = {
                     collapsable: false,
                     children: [
                         ['/', 'Introduction'],
-                        ['installation', 'Installation / Upgrading'],
-                        'support'
+                        ['/requirements', 'Requirements'],
+                        ['/installation', 'Installation / Upgrading'],
+                        ['/support', 'Support'],
                     ]
                 }
             ]
         }
     },
     markdown: {
-        anchor: {
-            level: [2, 3, 4]
-        },
-        toc: {
-            includeLevel: [3]
+        anchor: { level: [2, 3, 4] },
+        toc: { includeLevel: [3] },
+        config(md) {
+            md.use(require('vuepress-theme-flipbox/markup'))
         }
     }
 }
